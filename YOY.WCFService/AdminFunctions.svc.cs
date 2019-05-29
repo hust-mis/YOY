69ef324cb8bfb3ca1bfa8d707f6ecfa32841d8cd
若昕 王 <1121050482@qq.com>
2019-05-29 23:41:06 +0800

    新增管理员功能服务，web.config文件已修改。

---
+++ YOY.WCFService/AdminFunctions.svc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using YOY.BLL;
+using YOY.DAL;
+using YOY.Model.DB;
+using YOY.Model;
+
+namespace YOY.WCFService
+{
+    /// <summary>
+    /// 管理员操作接口实现
+    /// </summary>
+    public class AdminFunctions : IAdminFunctions
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
---
+++ YOY.WCFService/IAdminFunctions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using YOY.Model.DB;
+
+namespace YOY.WCFService
+{
+    
+    /// <summary>
+    /// 管理员操作接口声明
+    /// </summary>
+    [ServiceContract]
+    public interface IAdminFunctions
+    {
+        [OperationContract]
+        void DoWork();
+    }
+}
