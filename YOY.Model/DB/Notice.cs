9847d4b2bbaf065fc3b10d20b2bf315d5d4b4751
翀 王 <974402134@qq.com>
2019-05-29 21:04:02 +0800

    RFID处理写了一半

--- YOY.Model/DB/Notice.cs
+++ YOY.Model/DB/Notice.cs
@@ -25,38 +25,77 @@ namespace YOY.Model.DB
         public string NoticeID { get; set; }
 
         /// <summary>
-        /// 通知标题
+        /// 若为空，则为管理员发布的信息，若为正常ID，则为游客发布信息
         /// </summary>
-        [Column("NoticeTitle")]
+        [Column("VisitorID")]
         [DataMember]
-        public string NoticeTitle { get; set; }
+        public string VisitorID { get; set; }
 
         /// <summary>
-        /// 发布时间,YYYY-MM-DD Hh:mm:ss
+        /// 通知类型:
+        /// 0：寻人通知
+        /// 1：寻物通知
+        /// 2：活动通知
+        /// 3：园内提醒
+        /// </summary>
+        [Column("NoticeType")]
+        [DataMember]
+        public int NoticeType { get; set; }
+
+        /// <summary>
+        /// 发布时间
+        /// YYYY-MM-DD hh:mm:ss
         /// </summary>
         [Column("NoticeTime")]
         [DataMember]
         public DateTime NoticeTime { get; set; }
 
         /// <summary>
-        /// 内容时间，hh:mm:ss-hh:mm:ss
+        /// 事件发生时间
+        /// hh:mm:ss-hh:mm:ss
         /// </summary>
-        [Column("ContentTime")]
+        [Column("OccurTime")]
         [DataMember]
-        public string ContentTime { get; set; }
+        public string OccurTime { get; set; }
 
         /// <summary>
-        /// 通知地点
+        /// 事件发生地点
         /// </summary>
-        [Column("NoticeAddress")]
+        [Column("OccurAddress")]
         [DataMember]
-        public string NoticeAddress { get; set; }
+        public string OccurAddress { get; set; }
 
         /// <summary>
-        /// 通知内容
+        /// 事件详细描述
         /// </summary>
-        [Column("NoticeInfo")]
+        [Column("NoticeDetail")]
         [DataMember]
-        public string NoticeInfo { get; set; }
+        public string NoticeDetail { get; set; }
+
+        /// <summary>
+        /// 通知状态
+        /// 0：未审核
+        /// 1：审核通过
+        /// 2：审核拒绝
+        /// </summary>
+        [Column("NoticeStatus")]
+        [DataMember]
+        public int NoticeStatus { get; set; }
+
+        /// <summary>
+        /// 审核备注:未通过原因
+        /// </summary>
+        [Column("Remarks")]
+        [DataMember]
+        public string Remarks { get; set; }
+
+        /// <summary>
+        /// 审核时间
+        /// YYYY-MM-DD Hh:mm:ss
+        /// </summary>
+        [Column("CheckTime")]
+        [DataMember]
+        public DateTime CheckTime { get; set; }
+
     }
 }
